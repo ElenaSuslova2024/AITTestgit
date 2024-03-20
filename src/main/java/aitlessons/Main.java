c3c2c7749a7ea29e88a3851e1d47852fb77d3678
ElenaSuslova2024 <160174936+ElenaSuslova2024@users.noreply.github.com>
2024-03-20 20:23:30 +0100

    class Main was added

---
+++ src/main/java/aitlessons/Main.java
@@ -0,0 +1,12 @@
+package aitlessons;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a =  1;
+        java.lang.String hello = "aaa";
+
+
+
+    }
+}
